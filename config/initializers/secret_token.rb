6add451c7dee55f8f2de3164bb1d81e07a1197fa
sudheeshap <sudheeshap8@gmail.com>
2013-02-27 20:55:15 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Paintapp::Application.config.secret_token = 'edd5814ac88c0d080e073c390d19aaf1a3b9fd2ab0acd9e5c79f466c8cfe35c2400d6babd47e2b9cf14449d405e674043841b1ca5d90a091b119475ca0155652'
